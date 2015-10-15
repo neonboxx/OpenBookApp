32c3b207065dfc95a3c0780ba821b3050d09fcfb
John Bradshaw <neonboxx@gmail.com>
2015-10-15 08:23:03 +0100

    Initial

---
+++ src/OpenBookApp/wwwroot/Scripts/App.js
@@ -0,0 +1,29 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+define(["require", "exports", 'angular2/angular2'], function (require, exports, angular2_1) {
+    var AppComponent = (function () {
+        function AppComponent() {
+            this.name = "John Doe";
+        }
+        AppComponent.prototype.hello = function () {
+            alert("hello, " + this.name);
+        };
+        AppComponent = __decorate([
+            angular2_1.Component({
+                selector: 'my-app'
+            }),
+            angular2_1.View({
+                directives: [angular2_1.formDirectives],
+                templateUrl: 'mainForm.html'
+            })
+        ], AppComponent);
+        return AppComponent;
+    })();
+    angular2_1.bootstrap(AppComponent);
+});
---
+++ src/OpenBookApp/wwwroot/Scripts/App.ts
@@ -0,0 +1,19 @@
+﻿/// <reference path="../../typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap, formDirectives} from 'angular2/angular2';
+@Component({
+    selector: 'my-app'
+})
+@View({
+        directives: [formDirectives],
+    templateUrl: 'mainForm.html'
+})
+class AppComponent {
+    name: string;
+    hello(): void {
+        alert("hello, " + this.name);
+    }
+    constructor() {
+        this.name = "John Doe";
+    }
+}
+bootstrap(AppComponent);
